f3c7410a817becb9aca0371e5a3d7612d8856d22
jokulyao <forlive_yxs@qq.com>
2016-08-08 11:44:22 +0800

    项目转向idea测试提交

--- ch1_recipe01/src/com/packtpub/java7/concurrency/chapter1/recipe1/core/Main.java
+++ ch1_recipe01/src/com/packtpub/java7/concurrency/chapter1/recipe1/core/Main.java
@@ -13,9 +13,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		//Launch 10 threads that make the operation with a different number
+
 		for (int i=1; i<=10; i++){
 			Calculator calculator=new Calculator(i);
+			//Launch 10 threads that make the operation with a different number
 			Thread thread=new Thread(calculator);
 			thread.start();
 		}
